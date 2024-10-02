35880344680b1f9814cdc6f11f0d9acc2c3f15e7
Vijay Singh Parihar <vsp.krashi@gmail.com>
2024-10-02 15:34:18 +0530

    updated urls

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const io = new Server(server,{
     cors:{
-        origin : "http://localhost:3001",
+        origin : "https://super-tic-tac-client.vercel.app/",
         methods : ["GET","POST"]
     }
 });
